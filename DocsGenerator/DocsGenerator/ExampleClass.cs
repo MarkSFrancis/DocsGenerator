98f1c31708b2f87153b645ac25b5a84da67ec9ad
Mark Francis <mrkfrancis8@gmail.com>
2018-08-31 11:16:01 +0100

    Created new DocsGenerator using the example theme from docfx

---
+++ DocsGenerator/DocsGenerator/ExampleClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DocsGenerator
+{
+    /// <summary>
+    /// Test
+    /// <code language="csharp">// This is sample code</code>
+    /// </summary>
+    public class ExampleClass
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets a sample name
+        /// </summary>
+        /// <param name="firstName">The person's first name</param>
+        /// <param name="lastName">The person's last name</param>
+        /// <param name="middleNames">A <see cref="string[]"/> of all the person's middle names</param>
+        /// <returns>The name of the current person</returns>
+        /// <remarks>Puts a space between each name</remarks>
+        /// <example>
+        /// <code language="lang-csharp">
+        /// var example = new ExampleClass();
+        /// var fullName = example.GetFullName("John", "Smith", "David", "James");
+        /// 
+        /// // Output: "John David James Smith"
+        /// Console.WriteLine(fullName);
+        /// </code>
+        /// </example>
+        public string GetFullName(string firstName, string lastName, params string[] middleNames)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
